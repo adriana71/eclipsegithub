82839efb6d0755c4f1d94c9938b93170f50461b9
adriana71 <88009074+adriana71@users.noreply.github.com>
2026-01-30 08:39:23 -0600

    Este es el tercer commit

--- HolaGitHub2/src/Principal.java
+++ HolaGitHub2/src/Principal.java
@@ -3,8 +3,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int numero=0;
+		
 		System.out.println("Hola Mundo");
 		System.out.println("Hola Java, Eclipse y Github");
+		System.out.println(numero+2);
 	}
 
 }
